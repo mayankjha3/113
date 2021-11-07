c7ef88f1bd9f88e1ed780f3464d28854ff4f9477
mayankjha3 <84058462+mayankjha3@users.noreply.github.com>
2021-11-07 16:20:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640 , 480);
+    canvas.position(150 , 150);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video , 230 , 150 , 200 , 200 );
+
+
+    fill(0,128,0);
+    stroke(0,128,0);
+    rect(90,40,460,20);
+
+    rect(90,420,460,20);
+
+
+}
